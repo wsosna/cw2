28ba88348c88a1d8e0b1f32ae35a90075b2f674e
wsosna <weronikasosnowska97@gmail.com>
2020-03-29 16:34:04 +0200

    Zadanie
    Dodaję rozwiązania

---
+++ WebApp/Controllers/StudentsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.DAL;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if(id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id ==2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono zasobu");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Models.Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            return Ok("Aktualizacja zakończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie zakończone");
+        }
+    }
+}
---
+++ WebApp/DAL/MockDbService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Models.Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Models.Student>
+            {
+                new Models.Student{IdStudent=1, FirstName="Jan", LastName="Kowalski" },
+                new Models.Student{IdStudent=2, FirstName="Anna", LastName="Malewski" },
+                new Models.Student{IdStudent=3, FirstName="Andrzej", LastName="Andrzejewicz" }
+            };
+        }
+
+        public IEnumerable<Models.Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
